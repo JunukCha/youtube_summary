baebfc8d50568e7dcb29dfa776f24526a59babf9
unist <jucha@unist.ac.kr>
2024-07-20 16:31:31 +0900

    language dict & price_info

---
+++ lib/constants.py
@@ -0,0 +1,8 @@
+language_dict = {"korean": "ko", "english": "en"}
+
+price_info = {
+    "gpt-4o-mini": {"input": 0.150 / 1_000_000, "output": 0.600 / 1_000_000},
+    "gpt-4o": {"input": 5.00 / 1_000_000, "output": 15.00 / 1_000_000},
+    "gpt-4-turbo": {"input": 10.00 / 1_000_000, "output": 30.00 / 1_000_000},
+    "gpt-3.5-turbo": {"input": 0.50 / 1_000_000, "output": 1.50 / 1_000_000},
+}
